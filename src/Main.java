aab41fe1d04e0ab71419ccbca64dc8ac3d3cb261
Molch <molchaml@mail.uc.edu>
2022-09-18 23:19:43 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        double spring = 13.28;
+        double summer = 80.23;
+        double fall = 29.39;
+        double winter = 30.40;
+        double total = spring + summer + fall + winter;
+        //output
+        System.out.println("Your price for spring is $" + spring + " your costs for summer are $" + summer + " your costs for the fall $" + fall + " your costs for the winter are $" + winter + " your overall costs are $" + total);
+    }
+}
